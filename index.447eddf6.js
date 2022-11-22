4c5ce454da80470ad864b15796b0e5bdbda9b297
MakrFrost <101950285+MakrFrost@users.noreply.github.com>
2022-11-22 18:34:26 +0000

    Deploying to gh-pages from @ MakrFrost/frost-player@8fa1172027efa8272d7bf8a1cc6e4c390a3e8f08 🚀

---
+++ index.447eddf6.js
@@ -0,0 +1,2 @@
+const e=new Audio;document.querySelector(".catalog__item").addEventListener("click",(()=>{var o;o="../audio/Adele - Hello.mp3",e.src=o,e.play(),console.log("нажал на лишку плэй")}));
+//# sourceMappingURL=index.447eddf6.js.map
---
+++ index.a4b08333.js
@@ -0,0 +1,2 @@
+!function(){var e=new Audio;document.querySelector(".catalog__item").addEventListener("click",(function(){var o;o="../audio/Adele - Hello.mp3",e.src=o,e.play(),console.log("нажал на лишку плэй")}))}();
+//# sourceMappingURL=index.a4b08333.js.map
