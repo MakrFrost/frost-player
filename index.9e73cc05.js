40145a20d1222fa313bc9ead49e347bc14e55c31
MakrFrost <101950285+MakrFrost@users.noreply.github.com>
2022-12-01 16:42:49 +0000

    Deploying to gh-pages from @ MakrFrost/frost-player@b04878f552fc74b88fba6757cac8856139e89a29 🚀

--- index.9e73cc05.js
+++
@@ -1,2 +0,0 @@
-!function(){var n=new Audio;document.querySelector(".player__icon-play").addEventListener("click",(function(){c("./2.mp3")}));var c=function(c){n.src=c,n.play(),console.log("нажал на кнопку плэй")}}();
-//# sourceMappingURL=index.9e73cc05.js.map
---
+++ index.bd0eda6c.js
@@ -0,0 +1,2 @@
+!function(){var o=new Audio,l=document.querySelector(".player__icon-play");l.addEventListener("click",(function(){var l;l="/1.mp3",console.log(o.src=l),console.log(o.load()),console.log(o.play()),console.log("нажал на кнопку плэй")}))}();
+//# sourceMappingURL=index.bd0eda6c.js.map
