24c7ceb7e9d1fe981a154bee3d6d34e83251c0f9
MakrFrost <frostmark@ukr.net>
2022-12-13 16:36:47 +0200

    Add  js+buttons

--- src/js/Main.js
+++ src/js/Main.js
@@ -6,22 +6,26 @@ const trackCard = document.getElementsByClassName('catalog__item');
 const buttonPause = document.querySelector('.player__controller-paused');
 const buttonPlay = document.querySelector('.player__controller-play');
 const buttonStop = document.querySelector('.player__controller-stop');
+const playerBar = document.querySelector('.player');
 
-buttonPause.addEventListener('click', () => {
-  PlayerControllers();
-});
 for (const track of trackCard) {
   track.addEventListener('click', PlayMusic);
 }
-buttonStop.addEventListener('click', () => {
-  console.log('вы стопанули музыку');
+buttonPause.addEventListener('click', () => {
+  PlayerControllers();
 });
+buttonStop.addEventListener('click', StopMusic);
 
 function PlayMusic(e) {
-  //   audio.src = e.currentTarget;
-  //   console.log(e.currentTarget);
-  //   audio.play();
-  console.log('запустили музыку');
+  for (const track of trackCard) {
+    track.classList.remove('catalog__active');
+  }
+  audio.src = e.currentTarget;
+  audio.play();
+  playerBar.classList.add('player-active');
+  buttonPlay.classList.remove('player__icon-play');
+  e.currentTarget.classList.add('catalog__active');
+  console.log('вы открыли плеер, и запустили музыку');
 }
 function PlayerControllers() {
   if (audio.paused) {
@@ -32,5 +36,13 @@ function PlayerControllers() {
     buttonPlay.classList.add('player__icon-play');
     console.log('остановили музыку');
     audio.pause();
+  }
+}
+function StopMusic(e) {
+  console.log('вы остановили музыку и скрыли плеер');
+  audio.src = '';
+  playerBar.classList.remove('player-active');
+  for (const track of trackCard) {
+    track.classList.remove('catalog__active');
   }
 }
