51c79e28adc14c9e2e3f26280c0ed727397c66ad
MakrFrost <frostmark@ukr.net>
2022-12-13 15:37:43 +0200

    add js

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,44 +1,36 @@
-// // import PlayMusic from './Components/PlayMusic';
+// import PlayMusic from './Components/PlayMusic';
 
-// const audio = new Audio();
+const audio = new Audio();
 
-// const trackCard = document.getElementsByClassName('catalog__item');
-// // const buttonPlay = document.querySelector('.player__icon-play');
+const trackCard = document.getElementsByClassName('catalog__item');
+const buttonPause = document.querySelector('.player__controller-paused');
+const buttonPlay = document.querySelector('.player__controller-play');
+const buttonStop = document.querySelector('.player__controller-stop');
 
-// const buttonPause = document.querySelector('.player__controller-paused');
-// const buttonPlay = document.querySelector('.player__controller-play');
-// console.log(buttonPause);
-// // console.log(buttonPlay);
+buttonPause.addEventListener('click', () => {
+  PlayerControllers();
+});
+for (const track of trackCard) {
+  track.addEventListener('click', PlayMusic);
+}
+buttonStop.addEventListener('click', () => {
+  console.log('вы стопанули музыку');
+});
 
-// buttonPause.addEventListener('click', () => {
-//   if (audio.paused) {
-//     // buttonPause.classList.remove('player____controller-play');
-//     buttonPause.innerHTML = '';
-
-//     console.log('убрали кнопку паузы');
-//     audio.play();
-//   } else {
-//     buttonPause.classList.add('player__controller-play');
-//     console.log('добавили кнопку паузы');
-//     audio.pause();
-//   }
-// });
-
-// for (const track of trackCard) {
-//   track.addEventListener('click', () => console.log('zapuskaem'));
-// }
-// !
-// const PlayMusic = url => {
-//   console.log((audio.src = url));
-//   console.log(audio.load());
-//   console.log(audio.play());
-//   console.log('нажал на кнопку плэй');
-// };
-
-// buttonPlay.addEventListener('click', () => {
-//   PlayMusic('');
-// });
-
-// console.log('rabotaem player');
-
-// 25 05
+function PlayMusic(e) {
+  //   audio.src = e.currentTarget;
+  //   console.log(e.currentTarget);
+  //   audio.play();
+  console.log('запустили музыку');
+}
+function PlayerControllers() {
+  if (audio.paused) {
+    buttonPlay.classList.remove('player__icon-play');
+    console.log('запустили музыку');
+    audio.play();
+  } else {
+    buttonPlay.classList.add('player__icon-play');
+    console.log('остановили музыку');
+    audio.pause();
+  }
+}
