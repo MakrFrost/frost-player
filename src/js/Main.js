7011eb49b5c32e116d5f7da2b72a75efa1d59eb9
MakrFrost <frostmark@ukr.net>
2022-12-13 13:10:04 +0200

    Upd icons

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,28 +1,33 @@
-// import PlayMusic from './Components/PlayMusic';
+// // import PlayMusic from './Components/PlayMusic';
 
-const audio = new Audio();
+// const audio = new Audio();
 
-const trackCard = document.getElementsByClassName('catalog__item');
-// const buttonPlay = document.querySelector('.player__icon-play');
-const buttonPause = document.querySelector('.player__icon-pause');
-console.log(buttonPause);
+// const trackCard = document.getElementsByClassName('catalog__item');
+// // const buttonPlay = document.querySelector('.player__icon-play');
 
-buttonPause.addEventListener('click', () => {
-  if (audio.paused) {
-    audio.play();
-    buttonPause.classList.remove('player__icon-play');
-    console.log('udalili');
-  } else {
-    audio.pause();
-    buttonPause.classList.add('player__icon-play');
-    console.log('dobavili');
-  }
-});
+// const buttonPause = document.querySelector('.player__controller-paused');
+// const buttonPlay = document.querySelector('.player__controller-play');
+// console.log(buttonPause);
+// // console.log(buttonPlay);
 
-for (const track of trackCard) {
-  track.addEventListener('click', () => console.log('zapuskaem'));
-}
+// buttonPause.addEventListener('click', () => {
+//   if (audio.paused) {
+//     // buttonPause.classList.remove('player____controller-play');
+//     buttonPause.innerHTML = '';
 
+//     console.log('убрали кнопку паузы');
+//     audio.play();
+//   } else {
+//     buttonPause.classList.add('player__controller-play');
+//     console.log('добавили кнопку паузы');
+//     audio.pause();
+//   }
+// });
+
+// for (const track of trackCard) {
+//   track.addEventListener('click', () => console.log('zapuskaem'));
+// }
+// !
 // const PlayMusic = url => {
 //   console.log((audio.src = url));
 //   console.log(audio.load());
